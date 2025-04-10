cde52acf83b8c1f75f2955cc465af9fc0f0bede1
Beatriz Alves <bia.h.alves@hotmail.com>
2025-04-10 00:22:35 +0000

    Exemplos Array

---
+++ main.js
@@ -0,0 +1,13 @@
+let allisson_nota_av1 = 8;
+let allisson_nota_av2 = 9.6;
+let allisson_media = (allisson_nota_av1 + allisson_nota_av2) / 2;
+
+console.log ("Media Allisson: " + allisson_media);
+
+console.log("Exemplo com Arrays")
+let notas_allisson = [8,9.6, ];
+console.log ("AV1: " + notas_allisson[0]);
+console.log ("AV2: " + notas_allisson[1]);
+notas_allisson[2] = (notas_allisson[0] + notas_allisson[1]) / 2;
+console.log ("Media: " + notas_allisson[2]);
+console.log ("Todas: " + notas_allisson);
